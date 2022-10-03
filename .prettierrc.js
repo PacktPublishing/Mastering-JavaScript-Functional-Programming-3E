bcb8feb658a14e47a50425920651048b71e22cdc
Federico Kereki <fkereki@gmail.com>
2022-10-02 22:55:31 -0300

    Some tests

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
