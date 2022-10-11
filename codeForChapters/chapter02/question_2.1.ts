251061830de92ab148b035b9aa3b9a11a0ece621
Federico Kereki <fkereki@gmail.com>
2022-10-11 18:29:24 -0300

    Fix detail

--- codeForChapters/chapter02/question_2.1.ts
+++ codeForChapters/chapter02/question_2.1.ts
@@ -6,16 +6,4 @@ const once = <FNType extends (...args: any[]) => any>(
     fn = null;
   }) as FNType;
 
-const sum = (a: number, b: number) =>
-  console.log("SUM", a, b);
-
-const sumOnce = once(sum);
-
-console.log("A");
-sumOnce(3, 5); // output SUM 3 5
-console.log("B");
-sumOnce(3, 5); // no output
-console.log("C");
-sumOnce(3, 5); // no output
-
 export { once };
