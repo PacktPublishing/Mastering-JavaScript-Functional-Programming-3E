c5f213975bb2ed5ece0d49948fb369a5911b701e
Federico Kereki <fkereki@gmail.com>
2023-06-12 08:13:26 -0300

    Fix in case fn() returns an array

--- codeForChapters/chapter 05/map.ts
+++ codeForChapters/chapter 05/map.ts
@@ -1,6 +1,6 @@
 const myMap = <T, R>(arr: T[], fn: (x: T) => R): R[] =>
   arr.reduce(
-    (x: R[], y: T): R[] => x.concat(fn(y)),
+    (x: R[], y: T): R[] => x.concat([fn(y)]),
     [] as R[]
   );
 
